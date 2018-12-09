03db927ed281ff373405106963d167d1659e1ac9
nestorastsivras@gmail.com <nestorastsivras@gmail.com>
2018-12-09 23:54:13 +0200

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_09_162645) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
